f91e8b4cc6055717e0fb2dde37c376be9ba0176a
s-dimitrova <satanasova84@gmail.com>
2023-01-25 23:25:27 +0200

    Add project files.

---
+++ Collection/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Collection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var collection = new Collection<int>();
+            Console.WriteLine("Current collection: " + collection);
+
+            Console.WriteLine("Collection count: " + collection.Count);
+            Console.WriteLine("Collection capacity: " + collection.Capacity);
+
+            collection.Add(5);
+            Console.Writeine("Collection collection: " + collection);
+
+            collection.AddRange(6, 7);
+
+            Console.WriteLine("Current collection: " + collection);
+
+            Console.WriteLine)("Print the firs element: " + collection[0]);
+
+            collection[0] = 55;
+            Console.WriteLine("Print the firs element: " + collection[0]);
+
+            collection.InsertAt(2, 666);
+
+            Console.WriteLine("Current collection: " + collection);
+
+            collection.Excange(1, 2);
+            Console.WriteLine("Current collection: " + collection);
+
+            collection.Clear();
+            Console.WriteLine("Current collection: " + collection);
+
+
+
+
+        }
+    }
+}
